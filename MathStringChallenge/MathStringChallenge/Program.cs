fb2f286ae45bc282c1e9088f84fd035cb1c7c63e
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-19 22:51:50 -0700

    commit

---
+++ MathStringChallenge/MathStringChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathStringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Math operation on string
+            //string firstName = "Jesse";
+            //string lastName = "Johnson";
+            //Console.WriteLine(firstName + lastName);
+
+            // implicat conversion of data type
+            //string name = "Jesse";
+            //int number = 5;
+            //Console.WriteLine(name + number);
+        }
+    }
+}
