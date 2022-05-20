343d7523c087e2a4d14b0a84b25a883fb3c1bf91
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-19 22:39:21 -0700

    commit

---
+++ MathProject/MathProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Addition
+            //int num1 = 9;
+            //int num2 = 5;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+
+            // Subtraction
+            //int num1 = 8;
+            //int num2 = 4;
+            //int difference = num1 - num2;
+            //Console.WriteLine(difference);
+
+            // Multiplication
+            //int num1 = 5;
+            //int num2 = 7;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+
+            // Division
+            //int num1 = 12;
+            //int num2 = 3;
+            //int quotient = num1 / num2;
+            //Console.WriteLine(quotient);
+
+            // Modulus Operator
+            //int num1 = 7;
+            //int num2 = 3;
+            //int remainder = num1 % num2;
+            //Console.WriteLine(remainder);
+
+            // Determining if Even or Odd
+            //int number = 7;
+            //int remainder = number % 2;
+            //Console.WriteLine(remainder);
+        }
+    }
+}
