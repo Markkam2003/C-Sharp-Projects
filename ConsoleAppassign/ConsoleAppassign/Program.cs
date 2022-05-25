feca9ec227b29d51062b6afee1553e9b86a3003d
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-24 20:19:47 -0700

    commit

--- ConsoleAppassign/ConsoleAppassign/Program.cs
+++ ConsoleAppassign/ConsoleAppassign/Program.cs
@@ -10,12 +10,11 @@ namespace ConsoleAppassign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess what age the teacher is? ");
+            Console.WriteLine("Guess the age the teacher is?");
             int numberguess = Convert.ToInt32(Console.ReadLine());
             bool guessednumber = numberguess == 34;
 
-            // This is a do statement
-            do
+            while (!guessednumber)
             {
                 switch (numberguess)
                 {
@@ -25,12 +24,12 @@ namespace ConsoleAppassign
                         numberguess = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 29:
-                        Console.WriteLine("You guessed red. Try again.");
+                        Console.WriteLine("You guessed 29. Try again.");
                         Console.WriteLine("Guess what age the teacher is? ");
                         numberguess = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 38:
-                        Console.WriteLine("You guessed yellow. Try again.");
+                        Console.WriteLine("You guessed 38. Try again.");
                         Console.WriteLine("Guess what age the teacher is? ");
                         numberguess = Convert.ToInt32(Console.ReadLine());
                         break;
@@ -45,10 +44,48 @@ namespace ConsoleAppassign
                         break;
                 }
             }
-            // this is the while statement 
-            while (!guessednumber);
-
             Console.Read();
+        }
+
+            //Console.WriteLine("Guess what age the teacher is? ");
+            //int numberguess = Convert.ToInt32(Console.ReadLine());
+            //bool guessednumber = numberguess == 34;
+
+            //// This is a do statement
+            //do
+            //{
+            //    switch (numberguess)
+            //    {
+            //        case 24:
+            //            Console.WriteLine("You guessed 24. Try again.");
+            //            Console.WriteLine("Guess what age the teacher is? ");
+            //            numberguess = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("You guessed 29. Try again.");
+            //            Console.WriteLine("Guess what age the teacher is? ");
+            //            numberguess = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 38:
+            //            Console.WriteLine("You guessed 38. Try again.");
+            //            Console.WriteLine("Guess what age the teacher is? ");
+            //            numberguess = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 34:
+            //            Console.WriteLine("You guessed the teachers age. That is correct.");
+            //            guessednumber = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are wrong.");
+            //            Console.Write("Guess a number?");
+            //            numberguess = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            //// this is the while statement 
+            //while (!guessednumber);
+
+            //Console.Read();
         }
     }
 }
