f2e9da2d0d6e87b5a900e0d10ea2933503c0785f
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-26 15:17:15 -0700

    commit

---
+++ OverloadMethodassign/OverloadMethodassign/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodassign
+{
+    public class Math
+    {
+        //void: division operation
+        public void myMethod(int userNum)
+        {
+            Console.WriteLine("Your number divided by two is: " + (userNum / 2));
+        }
+        //static: addition operation
+        public static int myMethod(int userNum, int myNum)
+        {
+            int newNum = userNum + myNum;
+            return newNum;
+        }
+
+        //using out parameter 
+        public void MethodOut(int userNum, out int extraNum)
+        {
+            extraNum = userNum + 1;
+        }
+    }
+}
---
+++ OverloadMethodassign/OverloadMethodassign/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int returnMult;
+
+            //instantiate MathOp class
+            Math myNumObj = new Math();
+
+            //user input is divided by 2 and output is displayed
+            Console.WriteLine("Input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            myNumObj.myMethod(userNum);
+
+            //static overload method with an out parameter 
+            int myNum = 1;
+            Console.WriteLine("Your number plus my number is: " + Math.myMethod(userNum, myNum));
+            myNumObj.MethodOut(userNum, out int extraNum);
+            Console.WriteLine("That number plus one is: " + extraNum);
+
+            returnMult = Staticmath.MethodMult(userNum);
+            Console.WriteLine("That number times five is: " + returnMult);
+
+            Console.ReadLine();
+        }
+    }
+}
