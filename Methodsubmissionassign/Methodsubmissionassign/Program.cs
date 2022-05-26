f7954a0900a94c11732c8d70c68756d066f421e8
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-26 13:21:04 -0700

    commit

--- Methodsubmissionassign/Methodsubmissionassign/Program.cs
+++ Methodsubmissionassign/Methodsubmissionassign/Program.cs
@@ -10,7 +10,31 @@ namespace Methodsubmissionassign
     {
         static void Main(string[] args)
         {
+            //takes int as parameter
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
 
+            //takes int as parameter (optional)
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+
+            //instantiate class
+            Math myNum = new Math();
+
+            //if two numbers - will add them otherwise it will return input int. 
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myNum.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myNum.AddInts(userNum);
+            }
+
+
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
         }
     }
 }
