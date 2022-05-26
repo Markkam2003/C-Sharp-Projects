ab833092e0496b7ede2523655ac4fbb4944c27c2
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-26 13:06:25 -0700

    commit

---
+++ MethodClassassign/MethodClassassign/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassassign
+{
+    public class Method
+    {
+        public void mathOp(int firstNum, int secondNum)
+        {
+            firstNum += 10;
+            Console.WriteLine("The second input is: " + secondNum);
+        }
+    }
+}
