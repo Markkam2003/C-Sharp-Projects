ae6e86c50809d529c7d71658334f774c57db19f6
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-27 09:59:18 -0700

    commit

---
+++ Polymorphismassign/Polymorphismassign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphismassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person1 = new Employee();
+            person1.FirstName = "Bob";
+            person1.LastName = "Fisher";
+            person1.SayName();
+
+            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            person1.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
