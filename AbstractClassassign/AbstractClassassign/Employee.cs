e5bc5f524865a25f76cff3a2e735ff9eb7755731
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-26 23:03:38 -0700

    commit

---
+++ AbstractClassassign/AbstractClassassign/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassassign
+{
+    //inherit from the Person class
+    public class Employee : Person
+    {
+        //Implement the Sayname() method inside of the Employee class
+        public override void sayName()
+        {
+            Console.WriteLine("Employee name is: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClassassign/AbstractClassassign/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate an Employee object
+            Employee employee1 = new Employee();
+
+            employee1.firstName = "Bob";
+            employee1.lastName = "Fisher";
+            employee1.sayName();
+
+            Console.ReadLine();
+        }
+    }
+}
