a6180518498b2b24a207451a25e53177e2031686
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-26 21:48:06 -0700

    commit

---
+++ MethodsandObjectsassign/MethodsandObjectsassign/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsassign
+{
+    public class Employee : Person
+    {
+        // superclass property defintion 
+        int id { get; set; }
+    }
+}
