d9c19ef0bedcb47a38efdc5cb1d9113eb507df78
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-31 11:16:11 -0700

    commit

---
+++ TryCatchassign/TryCatchassign/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge = 0;
+            //Exceptions must be handled using “try/catch.”
+            try
+            {
+                //Ask the user for their age 
+                Console.WriteLine("What is your age?");
+                userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge == 0 || userAge < 0)
+                {
+                    throw new ExceptionHandling();
+                }
+                //Display the Year the user was born
+                int userBorn = DateTime.Now.Year - userAge;
+                Console.WriteLine("You were born in: " + userBorn);
+                Console.ReadLine();
+            }
+            catch (ExceptionHandling)
+            {
+                //Display appropriate error messages if the user enters zero or negative numbers.
+                Console.WriteLine("Please enter a number that is non zero and not negative.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                //Display a general message if an exception was caused by anything else.
+                Console.WriteLine("Something went wrong.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -8,13 +8,19 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            const string casinoName = "Grand Hotel and Casino";
 
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Lets start by tellin me your name.");
             string playerName = Console.ReadLine();
 
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today!");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
 
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
@@ -27,7 +33,22 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occured. Please contact your System Administrator");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
